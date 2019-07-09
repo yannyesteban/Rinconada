29ac8eb8182624650d05f212f6f9518e45b83557
Yanny Nunez <yannyesteban@hotmail.com>
2019-07-08 21:17:04 -0400

    ok

--- app/src/main/cpp/act/main.cpp
+++ app/src/main/cpp/act/main.cpp
@@ -8,7 +8,18 @@
 #include "FileRead.h"
 void android_main(struct android_app* app) {
 
+
+    FileRead::setAssetManager(app->activity->assetManager);
+    FileRead::setDataPath(app->activity->internalDataPath, app->activity->externalDataPath);
+
+FileRead::AA();
+
+
     FileRead F = FileRead();
+
+
+    F.print("hola.txt");
+    return;
     F.test(app->activity->assetManager, app->activity->externalDataPath, app->activity->internalDataPath);
     //F.test(app->activity->assetManager, app->activity->internalDataPath);
 //app->activity->env->
