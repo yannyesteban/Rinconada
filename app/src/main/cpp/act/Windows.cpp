bbf81dafdfe13f84932ef1247bfa6133e6f664a0
Yanny Núñez <yannyesteban@hotmail.com>
2019-08-05 00:19:50 -0400

    mesh

--- app/src/main/cpp/act/Windows.cpp
+++ app/src/main/cpp/act/Windows.cpp
@@ -1430,7 +1430,7 @@ void Windows::test1d() {
     glm::mat4 Projection = glm::perspective(glm::radians(45.0f), 4.0f / 4.0f, 0.1f, 100.0f);
     // Camera matrix
     glm::mat4 View       = glm::lookAt(
-            glm::vec3(0,0,3), // Camera is at (4,3,-3), in World Space
+            glm::vec3(0,0,8), // Camera is at (4,3,-3), in World Space
             glm::vec3(0,0,0), // and looks at the origin
             glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
     );
